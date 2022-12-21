38a9afd222e456d97e4cb61d8c09a950c786e183
franc <francisco.graya26@gmail.com>
2022-12-20 23:55:18 -0600

    se agregaron configuraciones principales al proyecto

---
+++ src/main/java/com/inventpro/invent/InventApplication.java
@@ -0,0 +1,13 @@
+package com.inventpro.invent;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InventApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(InventApplication.class, args);
+	}
+
+}
